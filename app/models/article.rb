e7c83a154080ba0adf795022757184b8095c233d
AliSiddique <alisiddique10@hotmail.com>
2024-01-12 23:26:43 +0000

    DA

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,5 @@
 class Article < ApplicationRecord
+    validates :title, presence:true
+    validates :body,presence:true,length:{minimum:20}
+    has_many :comments, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,9 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
   root "articles#index"
- resources :articles
+  resources :articles do
+    resources :comments
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
